63249eb46b1292c818cd6d71263287765251adeb
Jun Rey Ellezo <junreyellezo.cpe@gmail.com>
2021-02-25 22:29:36 +0800

    feat: update box size

--- src/components/TimerBoxComp.tsx
+++ src/components/TimerBoxComp.tsx
@@ -8,7 +8,7 @@ interface ComponentProps {
 function TimerBoxComp(props: ComponentProps) {
   return (
     <div className="space-y-4">
-      <div className="relative h-40 w-40 bg-darkest-blue rounded-lg">
+      <div className="relative h-36 w-36 bg-darkest-blue rounded-lg">
         <div className="absolute top-0 left-0 right-0 bottom-2 rounded-lg">
           <div className="h-full flex flex-col justify-between space-y-px">
             <div className="relative h-full w-full bg-dark-blue opacity-75 rounded-tl-lg rounded-tr-lg card__top"></div>
--- src/components/TimerComp.tsx
+++ src/components/TimerComp.tsx
@@ -13,7 +13,7 @@ function TimerComp() {
   } = useTimer();
 
   return (
-    <div className="grid grid-cols-1 auto-rows-max gap-8 md:grid-cols-4 xs:grid-cols-2">
+    <div className="grid grid-cols-2 auto-rows-max gap-8 md:grid-cols-4">
       <TimerBoxComp value={day} label="days" />
       <TimerBoxComp value={hour} label="hours" />
       <TimerBoxComp value={minute} label="minutes" />
